4596b9e946d396bcff4474e5567bf083be1919dd
jm4887 <juhikarwa@gmail.com>
2024-12-05 20:05:22 -0500

    added openingHours

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -4,10 +4,12 @@ const apiRouter = express.Router();
 const userRouter = require("./controllers/userController.js");
 const trucksRouter = require("./controllers/trucksController.js");
 const menuRouter = require("./controllers/menuController.js");
+const openingHoursRouter = require("./controllers/openingHoursController.js");
 
 
 apiRouter.use("/users", userRouter);
 apiRouter.use("/trucks", trucksRouter);
 apiRouter.use("/menu", menuRouter);
+apiRouter.use("/openingHours", openingHoursRouter);
 
 module.exports = apiRouter;
---
+++ src/app/api/services/openingHoursService.js
@@ -0,0 +1,19 @@
+const pool = require("./connection.js");
+
+const getOpeningHoursByTruckId = async (truckId) => {
+    try {
+        const query = `
+            SELECT * FROM OpeningHours
+            WHERE truckId = $1
+        `;
+        const result = await pool.query(query, [truckId]);
+        return result.rows;  // Return the menu items along with dietary restrictions
+    } catch (error) {
+        console.error("Error fetching opening hours for truck:", error);
+        throw new Error("Error fetching opening hours");
+    }
+};
+
+module.exports = {
+    getOpeningHoursByTruckId
+};
